beba5f6e60a5e7714d88d6dd14d2890e74f20b5f
John (Juan) Lema <jlema@users.noreply.github.com>
2020-07-26 21:37:36 -0700

    working as per task 1 functional requirements

--- src/Board.js
+++ src/Board.js
@@ -28,23 +28,23 @@ export default class Board extends React.Component {
   }
   getClients() {
     return [
-      ['1','Stark, White and Abbott','Cloned Optimal Architecture', 'in-progress'],
-      ['2','Wiza LLC','Exclusive Bandwidth-Monitored Implementation', 'complete'],
+      ['1','Stark, White and Abbott','Cloned Optimal Architecture', 'backlog'],
+      ['2','Wiza LLC','Exclusive Bandwidth-Monitored Implementation', 'backlog'],
       ['3','Nolan LLC','Vision-Oriented 4Thgeneration Graphicaluserinterface', 'backlog'],
-      ['4','Thompson PLC','Streamlined Regional Knowledgeuser', 'in-progress'],
-      ['5','Walker-Williamson','Team-Oriented 6Thgeneration Matrix', 'in-progress'],
+      ['4','Thompson PLC','Streamlined Regional Knowledgeuser', 'backlog'],
+      ['5','Walker-Williamson','Team-Oriented 6Thgeneration Matrix', 'backlog'],
       ['6','Boehm and Sons','Automated Systematic Paradigm', 'backlog'],
       ['7','Runolfsson, Hegmann and Block','Integrated Transitional Strategy', 'backlog'],
       ['8','Schumm-Labadie','Operative Heuristic Challenge', 'backlog'],
       ['9','Kohler Group','Re-Contextualized Multi-Tasking Attitude', 'backlog'],
       ['10','Romaguera Inc','Managed Foreground Toolset', 'backlog'],
-      ['11','Reilly-King','Future-Proofed Interactive Toolset', 'complete'],
+      ['11','Reilly-King','Future-Proofed Interactive Toolset', 'backlog'],
       ['12','Emard, Champlin and Runolfsdottir','Devolved Needs-Based Capability', 'backlog'],
-      ['13','Fritsch, Cronin and Wolff','Open-Source 3Rdgeneration Website', 'complete'],
+      ['13','Fritsch, Cronin and Wolff','Open-Source 3Rdgeneration Website', 'backlog'],
       ['14','Borer LLC','Profit-Focused Incremental Orchestration', 'backlog'],
-      ['15','Emmerich-Ankunding','User-Centric Stable Extranet', 'in-progress'],
-      ['16','Willms-Abbott','Progressive Bandwidth-Monitored Access', 'in-progress'],
-      ['17','Brekke PLC','Intuitive User-Facing Customerloyalty', 'complete'],
+      ['15','Emmerich-Ankunding','User-Centric Stable Extranet', 'backlog'],
+      ['16','Willms-Abbott','Progressive Bandwidth-Monitored Access', 'backlog'],
+      ['17','Brekke PLC','Intuitive User-Facing Customerloyalty', 'backlog'],
       ['18','Bins, Toy and Klocko','Integrated Assymetric Software', 'backlog'],
       ['19','Hodkiewicz-Hayes','Programmable Systematic Securedline', 'backlog'],
       ['20','Murphy, Lang and Ferry','Organized Explicit Access', 'backlog'],
@@ -83,6 +83,9 @@ export default class Board extends React.Component {
       this.setState({laneTo: this.getDOMIndex(target.parentNode.parentNode)});
       this.setState({rowTo : this.getDOMIndex(el)});
 
+      // the cancel method will gracefully cancel the drag action on an element managed by drake
+      this.drake.cancel(true);
+
       const clientsFromKey = Object.keys(this.state.clients)[this.state.laneFrom];
       let clientsFromCopy = [...this.state.clients[clientsFromKey]];
 
@@ -96,14 +99,7 @@ export default class Board extends React.Component {
         clientsToKey === 'inProgress' ? item.status = 'in-progress' : item.status = clientsToKey;
         let clientsToCopy = [...this.state.clients[clientsToKey]];
         clientsToCopy.splice(this.state.rowTo, 0, item);
-
-        this.setState(prevState => {
-              return (
-                { clients: { ...prevState.clients, [clientsFromKey]: prevState.clients[clientsFromKey].filter(client => client.id !== item.id) } }
-              );
-            } 
-          ); //this line blows
-        this.setState({ clients: { ...this.state.clients, [clientsToKey]: clientsToCopy } });
+        this.setState({clients: { ...this.state.clients, [clientsFromKey]: clientsFromCopy, [clientsToKey]: clientsToCopy }});
       }
       
     });
